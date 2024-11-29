065a9afbbb8933457f598c603df53299949b93ea
root <root@MMENYENE-IDIONG>
2024-11-29 01:25:44 +0100

    9-api

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,34 +1,20 @@
-{
-  "name": "8-api",
-  "version": "1.0.0",
-  "description": "A simple API for learning basic integration testing in NodeJS.",
-  "main": "api.js",
-  "author": "Bezaleel Olakunori <bezaleeloci@gmail.com>",
-  "private": true,
-  "license": "MIT",
-  "scripts": {
-    "test": "node ./node_modules/mocha/bin/mocha"
-  },
-  "dependencies": {
-    "express": "^4.17.1"
-  },
-  "devDependencies": {
-    "chai": "^4.2.0",
-    "mocha": "^6.2.2",
-    "request": "^2.88.0",
-    "sinon": "^7.5.0"
-  },
-  "repository": {
-    "type": "git",
-    "url": "https://github.com/B3zaleel/alx-backend-javascript"
-  },
-  "bugs": {
-    "url": "https://github.com/B3zaleel/alx-backend-javascript/issues"
-  },
-  "homepage": "https://github.com/B3zaleel/alx-backend-javascript/tree/main/0x06-unittests_in_js/8-api",
-  "engines": {
-    "node": "16.x",
-    "npm": "8.x",
-    "yarn": "1.x"
-  }
-}
+const express = require('express');
+
+const app = express();
+const PORT = 7865;
+
+app.get('/', (_, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
+
+module.exports = app;
